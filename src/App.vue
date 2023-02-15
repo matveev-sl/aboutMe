b5ff320e7ebbb13cee91197f056b327efcda8c64
pavelmikheev <pavel@fluid.ch>
2023-02-15 21:36:25 +0400

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
+    <AppHeader />
     <router-view />
   </div>
 </template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: { AppHeader },
+};
+</script>
 
 <style lang="scss">
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-header">
+    hi
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AppHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+
+  .app-header {
+    color: red;
+  }
+
+</style>
